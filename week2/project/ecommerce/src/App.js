40a336ffb932d13c974d8e11cdad6c712aa42d40
HebaSalem <puupiiiii@gmail.com>
2022-11-19 18:12:52 +0100

    update app.js file

--- week2/project/ecommerce/src/App.js
+++ week2/project/ecommerce/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Categories from './components/Categories';
 import Products from './components/Products';
 import ProductContext from './hooks/context';
 
 function App() {
   return (
     <ProductContext>
+      <h1 className="title">Products</h1>
+      <Categories />
       <Products />
     </ProductContext>
   );
