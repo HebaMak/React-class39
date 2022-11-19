125ce91a5d020f68be9cf402767dc7791aa02b0f
HebaSalem <puupiiiii@gmail.com>
2022-11-20 00:31:30 +0100

    edit the loading and error messages to be dynamic

--- week2/project/ecommerce/src/components/Error.jsx
+++ week2/project/ecommerce/src/components/Error.jsx
@@ -1,8 +1,8 @@
 
-function Error() {
+function Error({title}) {
   return (
     <div className='error'>
-      <h2>Error in Fetching data Apple</h2>
+      <h2>{title}</h2>
       <img src="https://res.cloudinary.com/hapiii/image/upload/v1668878616/Error%20imgs/rixenux81h6bhjl5eq0n.gif" alt="error img" />
     </div>
   )
--- week2/project/ecommerce/src/components/Loading.jsx
+++ week2/project/ecommerce/src/components/Loading.jsx
@@ -1,7 +1,7 @@
-function Loading() {
+function Loading({title}) {
   return (
     <div className='loading'>
-      <h2>Just count to 10 and Products will Come 😀</h2>
+      <h2>{title}</h2>
       <img src="https://res.cloudinary.com/hapiii/image/upload/v1648557059/loading%20img/nopoaypkfxx3meexthwc.gif" alt="loading img" />
     </div>
   )
